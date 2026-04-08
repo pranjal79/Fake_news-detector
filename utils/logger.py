80b4308726ea6f070afcda5fdfcb73ec8dbe0e46
Pranjal Panigrahi <panigrahipranjal32@gmail.com>
2026-04-08 15:18:23 +0530

    Initial project setup

---
+++ utils/logger.py
@@ -0,0 +1,21 @@
+import logging
+import os
+
+def get_logger(name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        # Console handler
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.INFO)
+
+        # Format
+        formatter = logging.Formatter(
+            "[%(asctime)s] %(levelname)s - %(name)s - %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S"
+        )
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
+
+    return logger
